390973c30404cf86a9bae39f78e9616af6cda94e
HuitaePark <qkrgmlxo3174@gmail.com>
2026-06-08 03:15:34 +0900

    fix: 구글 태그매니저 적용 방식 개선

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 import { AppProvider } from "./context/AppContext";
 import ClientLayout from "./components/ClientLayout";
 import MixpanelInitializer from "./components/MixpanelInitializer";
@@ -124,16 +124,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko" suppressHydrationWarning>
+      <GoogleTagManager gtmId={GTM_ID} />
       <head>
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${GTM_ID}');
-          `}
-        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
